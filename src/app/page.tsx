f7e06ce80d774a2f475ec634f4b1104237488f91
Marianela Munoz Gutierrez <marianelamunozgutierrez@gmail.com>
2024-07-02 20:15:53 -0400

    Add fonts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
+import { titleFont } from "@/config/fonts";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center p-24">
+        <h1 className={ `${titleFont.className} font-bold `}>TesloShop</h1>
         <h1>TesloShop</h1>
     </main>
   );
