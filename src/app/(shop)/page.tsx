a71462ce4b7e14133c3833dba543b2dc4267eee2
Marianela Munoz Gutierrez <marianelamunozgutierrez@gmail.com>
2024-07-03 19:09:59 -0400

    Add initial project structure

---
+++ src/app/(shop)/page.tsx
@@ -0,0 +1,10 @@
+import { titleFont } from "@/config/fonts";
+
+export default function Home() {
+  return (
+    <div className="flex min-h-screen flex-col items-center p-24">
+      <h1 className={`${titleFont.className} font-bold `}>TesloShop</h1>
+      <h1>TesloShop</h1>
+    </div>
+  );
+}
---
+++ src/app/auth/new-account/page.tsx
@@ -0,0 +1,9 @@
+import { titleFont } from "@/config/fonts";
+
+export default function() {
+  return (
+    <div className="flex min-h-screen flex-col items-center p-24">
+      <h1>New Account Page</h1>
+    </div>
+  );
+}
