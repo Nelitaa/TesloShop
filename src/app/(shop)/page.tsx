974d25c2128bdc682248ef5d4424939bb5f4ab0b
Marianela Munoz Gutierrez <marianelamunozgutierrez@gmail.com>
2024-07-04 16:19:26 -0400

    Add title component

--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -1,10 +1,14 @@
+import { Title } from "@/components";
 import { titleFont } from "@/config/fonts";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen flex-col items-center p-24">
-      <h1 className={`${titleFont.className} font-bold `}>TesloShop</h1>
-      <h1>TesloShop</h1>
-    </div>
+    <>
+      <Title
+        title="Shop"
+        subtitle="Welcome to our shop"
+        className="mb-2"
+      />
+    </>
   );
 }
