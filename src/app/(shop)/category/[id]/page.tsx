c48b20ff302d856c41abe23905b540c6c5e27fe0
Marianela Munoz Gutierrez <marianelamunozgutierrez@gmail.com>
2024-07-03 22:32:01 -0400

    Add Not Found Page

--- src/app/(shop)/category/[id]/page.tsx
+++ src/app/(shop)/category/[id]/page.tsx
@@ -1,8 +1,21 @@
+import { notFound } from "next/navigation";
 
-export default function CategoryPage() {
+interface Props {
+  params: {
+    id: string;
+  };
+}
+
+export default function CategoryPage({ params }: Props) {
+  const { id } = params;
+
+  if (id === 'kids') {
+    notFound();
+  }
+
   return (
     <div>
-      <h1>CategoryPage</h1>
+      <h1>CategoryPage {id}</h1>
     </div>
   );
 }
