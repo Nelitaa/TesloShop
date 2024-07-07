6f52ebd311e9006cfe7d1075a8b0d340404b9618
Marianela Munoz Gutierrez <marianelamunozgutierrez@gmail.com>
2024-07-06 20:11:24 -0400

    Add product mobile slideshow

--- src/app/(shop)/product/[slug]/page.tsx
+++ src/app/(shop)/product/[slug]/page.tsx
@@ -1,4 +1,4 @@
-import { ProductSlideshow, QuantitySelector, SizeSelector } from "@/components";
+import { ProductMobileSlideshow, ProductSlideshow, QuantitySelector, SizeSelector } from "@/components";
 import { titleFont } from "@/config/fonts";
 import { initialData } from "@/seed/seed";
 import { notFound } from "next/navigation";
@@ -20,9 +20,17 @@ export default function ProductPage({ params }: Props) {
     <div className="mt-5 mb-20 grid grid-cols-1 md:grid-cols-3 gap-3">
       {/* Slideshow */}
       <div className="col-span-1 md:col-span-2">
+        {/* Desktop Slideshow */}
+        <ProductMobileSlideshow
+          title={product.title}
+          images={product.images}
+          className="block md:hidden"
+        />
+        {/* Desktop Slideshow */}
         <ProductSlideshow
           title={product.title}
           images={product.images}
+          className="hidden md:block"
         />
       </div>
 
