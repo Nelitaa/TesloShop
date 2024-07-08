9bac3381b219fabd8d2499ffd0df8bf0b07d293f
Marianela Munoz Gutierrez <marianelamunozgutierrez@gmail.com>
2024-07-08 12:22:08 -0400

    Add footer

---
+++ src/components/ui/footer/Footer.tsx
@@ -0,0 +1,16 @@
+import { titleFont } from '@/config/fonts';
+import Link from 'next/link';
+
+export const Footer = () => {
+  return (
+    <div className="flex w-full justify-center text-xs mb-10">
+      <Link href='/'>
+        <span className={`${titleFont.className} antialiased font-bold `}>TESLO </span>
+        <span>| Shop </span>
+        <span>© {new Date().getFullYear()}</span>
+      </Link>
+      <Link href='/' className="mx-3">PRIVACY & LEGAL</Link>
+      <Link href='/' className="mx-3">LOCATIONS</Link>
+    </div>
+  )
+}
