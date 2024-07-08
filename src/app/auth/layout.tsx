d3c3d1cc116ce6485b97089779ffed22476a4e9c
Marianela Munoz Gutierrez <marianelamunozgutierrez@gmail.com>
2024-07-08 15:59:09 -0400

    Add new account page

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -3,7 +3,7 @@ export default function ShopLayout({ children }: {
 }) {
   return (
     <main className="flex justify-center">
-      <div className="w-full sm:w-[350px] px-10">
+      <div className="w-full sm:w-[380px] px-10">
         {children}
       </div>
     </main>
