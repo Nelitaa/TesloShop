b731e9993e3fe1fd1c24f9f603390755cd09e0f3
Marianela Munoz Gutierrez <marianelamunozgutierrez@gmail.com>
2024-07-24 11:13:33 -0400

    Connect Login Form with NextAuth

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,3 +1,4 @@
 export { getPaginatedProductsWithImages } from './product/product-pagination';
 export { getProductBySlug } from './product/get-product-by-slug';
 export { getStockBySlug } from './product/get-stock-by-slug';
+export { authenticate } from './auth/login';
