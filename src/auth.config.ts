e8a6b1cebfd6bf70ea6588060ee46d3c4706e285
Marianela Munoz Gutierrez <marianelamunozgutierrez@gmail.com>
2024-07-24 15:37:00 -0400

    Validate user - NextAuth

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -1,6 +1,8 @@
 import NextAuth, { type NextAuthConfig } from 'next-auth';
 import Credentials from 'next-auth/providers/credentials';
 import { z } from 'zod';
+import prisma from './lib/prisma';
+import bcryptjs from 'bcryptjs';
 
 export const authConfig: NextAuthConfig = {
   pages: {
@@ -16,13 +18,16 @@ export const authConfig: NextAuthConfig = {
 
         if (!parsedCredentials.success) return null;
         const { email, password } = parsedCredentials.data;
-        console.log({ email, password });
 
         // Search the email
+        const user = await prisma.user.findUnique({ where: { email: email.toLowerCase() } });
+        if (!user) return null;
         // Compare passwords
-        // Return the user
-
-        return null;
+        if (!bcryptjs.compareSync(password, user.password)) return null;
+        // Return the user without the password
+        const { password: _, ...rest } = user;
+        console.log({ rest })
+        return rest;
       }
     }),
   ],
