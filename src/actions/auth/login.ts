3cdfe60c529a2c54000a1d89126da3b79fda92cb
Marianela Munoz Gutierrez <marianelamunozgutierrez@gmail.com>
2024-07-25 17:34:34 -0400

    Add redirect after login

--- src/actions/auth/login.ts
+++ src/actions/auth/login.ts
@@ -1,7 +1,7 @@
 'use server';
 
 import { signIn } from "@/auth.config";
-import { sleep } from "@/utils";
+// import { sleep } from "@/utils";
 
 export async function authenticate(
   prevState: string | undefined,
@@ -9,12 +9,15 @@ export async function authenticate(
 ) {
   try {
     // await sleep(2);
-    await signIn('credentials', Object.fromEntries(formData));
+    await signIn('credentials', {
+      ...Object.fromEntries(formData),
+      redirect: false,
+    });
+    return 'Success';
   } catch (error) {
-    // if ((error as Error).message.includes('CredentialsSignin')) {
-
-    // }
-    return 'CredentialsSignin';
-    // throw error;
+    if ((error as any).type === 'CredentialsSignin') {
+      return 'CredentialsSignin';
+    }
+    return 'Unknown error';
   }
 }
--- src/app/auth/login/ui/LoginForm.tsx
+++ src/app/auth/login/ui/LoginForm.tsx
@@ -3,12 +3,20 @@
 import { authenticate } from '@/actions';
 import clsx from 'clsx';
 import Link from 'next/link'
+import { useRouter } from 'next/navigation';
+
+import { useEffect } from 'react';
 import { useFormState, useFormStatus } from 'react-dom';
 import { IoInformationOutline } from 'react-icons/io5';
 
 export const LoginForm = () => {
   const [state, dispatch] = useFormState(authenticate, undefined);
-  console.log({ state });
+  const router = useRouter();
+  useEffect(() => {
+    if (state === "Success") {
+      router.replace("/");
+    }
+  }, [state, router]);
   return (
     <form action={dispatch} className="flex flex-col">
       <label htmlFor="email">Email</label>
