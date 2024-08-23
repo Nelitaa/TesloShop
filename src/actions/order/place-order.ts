624e43af1cb5b6403fe95aa9f2706d304a4f30e4
Marianela Munoz Gutierrez <marianelamunozgutierrez@gmail.com>
2024-08-23 19:52:48 -0400

    Add product retrieval and order total calculation logic in placeOrder function

--- src/actions/order/place-order.ts
+++ src/actions/order/place-order.ts
@@ -2,6 +2,7 @@
 
 import { auth } from "@/auth.config";
 import { Address, Size } from "@/interfaces";
+import prisma from "@/lib/prisma";
 
 interface ProductToOrder {
   productId: string;
@@ -9,7 +10,7 @@ interface ProductToOrder {
   size: Size;
 }
 
-export const placeOrder = async (productsIds: ProductToOrder[], address: Address) => {
+export const placeOrder = async (productIds: ProductToOrder[], address: Address) => {
   const session = await auth();
   const userId = session?.user.id;
   if (!userId) {
@@ -18,4 +19,33 @@ export const placeOrder = async (productsIds: ProductToOrder[], address: Address
       message: "User not authenticated",
     };
   }
-}
+
+  // Obtain products information
+  const products = await prisma.product.findMany({
+    where: {
+      id: {
+        in: productIds.map((p) => p.productId),
+      },
+    },
+  });
+
+  // Calculate total items
+  const itemsInOrder = productIds.reduce((count, p) => count + p.quantity, 0);
+
+  // Calculate total price, tax and subtotal price
+  const { subTotalPrice, tax, totalPrice } = productIds.reduce((totals, item) => {
+    const productQuantity = item.quantity;
+    const product = products.find((product) => product.id === item.productId);
+
+    if (!product) throw new Error(`${item.productId} Not found - 500`);
+
+    const subTotalPrice = product.price * productQuantity;
+
+    totals.subTotalPrice += subTotalPrice;
+    totals.tax += subTotalPrice * 0.15;
+    totals.totalPrice += subTotalPrice * 1.15;
+
+    return totals;
+
+  }, { subTotalPrice: 0, tax: 0, totalPrice: 0 });
+};
