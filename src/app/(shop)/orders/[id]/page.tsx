283e937056d764f57ddeda22022fcd907f77e0b9
Marianela Munoz Gutierrez <marianelamunozgutierrez@gmail.com>
2024-08-27 18:34:29 -0400

    Shorten the purchase order number

--- src/app/(shop)/orders/[id]/page.tsx
+++ src/app/(shop)/orders/[id]/page.tsx
@@ -19,13 +19,13 @@ interface Props {
 
 export default function OrderPage({ params }: Props) {
   const { id } = params;
-
+  // todo : get order by id server action 
   // todo: check
 
   return (
     <div className="flex justify-center items-center mb-72 px-10 sm:px-0">
       <div className="flex flex-col w-[1000px]">
-        <Title title={`Order #${id}`} />
+        <Title title={`Order #${id.split('-').at(-1)}`} />
         <div className="grid grid-cols-1 sm:grid-cols-2 gap-10">
 
           <div className="flex flex-col mt-5">
