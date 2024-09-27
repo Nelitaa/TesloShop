0e544189864df136edd062a7303ccb4e04ef7f82
Marianela Munoz Gutierrez <marianelamunozgutierrez@gmail.com>
2024-09-27 17:47:32 -0400

    Create product form layou

--- src/app/(shop)/admin/product/[slug]/ui/ProductForm.tsx
+++ src/app/(shop)/admin/product/[slug]/ui/ProductForm.tsx
@@ -1,8 +1,101 @@
+"use client";
 
-export const ProductForm = () => {
+import { Product } from "@/interfaces";
+
+interface Props {
+  product: Product;
+}
+
+const sizes = ["XS", "S", "M", "L", "XL", "XXL"];
+
+export const ProductForm = ({ product }: Props) => {
   return (
-    <form>
-      hello
+    <form className="grid px-5 mb-16 grid-cols-1 sm:px-0 sm:grid-cols-2 gap-3">
+      {/* Text */}
+      <div className="w-full">
+        <div className="flex flex-col mb-2">
+          <span>Name</span>
+          <input type="text" className="p-2 border rounded-md bg-gray-200" />
+        </div>
+
+        <div className="flex flex-col mb-2">
+          <span>Slug</span>
+          <input type="text" className="p-2 border rounded-md bg-gray-200" />
+        </div>
+
+        <div className="flex flex-col mb-2">
+          <span>Description</span>
+          <textarea
+            rows={5}
+            className="p-2 border rounded-md bg-gray-200"
+          ></textarea>
+        </div>
+
+        <div className="flex flex-col mb-2">
+          <span>Price</span>
+          <input type="number" className="p-2 border rounded-md bg-gray-200" />
+        </div>
+
+        <div className="flex flex-col mb-2">
+          <span>Tags</span>
+          <input type="text" className="p-2 border rounded-md bg-gray-200" />
+        </div>
+
+        <div className="flex flex-col mb-2">
+          <span>Gender</span>
+          <select className="p-2 border rounded-md bg-gray-200">
+            <option value="">[Select]</option>
+            <option value="men">Men</option>
+            <option value="women">Women</option>
+            <option value="kid">Kid</option>
+            <option value="Unisex">Unisex</option>
+          </select>
+        </div>
+
+        <div className="flex flex-col mb-2">
+          <span>Category</span>
+          <select className="p-2 border rounded-md bg-gray-200">
+            <option value="">[Select]</option>
+          </select>
+        </div>
+
+        <button className="btn-primary w-full">
+          Save
+        </button>
+      </div>
+
+      {/* Sizes and images */}
+      <div className="w-full">
+        {/* As checkboxes */}
+        <div className="flex flex-col">
+
+          <span>Sizes</span>
+          <div className="flex flex-wrap">
+
+            {
+              sizes.map(size => (
+                <div key={size} className="flex  items-center justify-center w-10 h-10 mr-2 border rounded-md">
+                  <span>{size}</span>
+                </div>
+              ))
+            }
+
+          </div>
+
+          <div className="flex flex-col mb-2">
+
+            <span>Images</span>
+            <input
+              type="file"
+              multiple
+              className="p-2 border rounded-md bg-gray-200"
+              accept="image/png, image/jpeg"
+            />
+
+          </div>
+
+        </div>
+      </div>
     </form>
   );
 };
